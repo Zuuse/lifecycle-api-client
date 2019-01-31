f7770e02605e01a4cebb7840ef891bc70ad0adab
Adrian Akison <adrian.akison@zuuse.com>
2019-01-31 14:55:15 +1000

    Manual fix to make endpoint always https

--- Zuuse.Lifecycle.Client/ZuuseLifecycleManagementApi.cs
+++ Zuuse.Lifecycle.Client/ZuuseLifecycleManagementApi.cs
@@ -148,7 +148,7 @@ namespace Zuuse.Lifecycle.Client
             Lifecycle = new Lifecycle(this);
             Scenario = new Scenario(this);
             Zuuse = new Zuuse(this);
-            BaseUri = new System.Uri("http://zuuselifecyclemanagementapidev.azurewebsites.net");
+            BaseUri = new System.Uri("https://zuuselifecyclemanagementapidev.azurewebsites.net");
             SerializationSettings = new JsonSerializerSettings
             {
                 Formatting = Newtonsoft.Json.Formatting.Indented,
